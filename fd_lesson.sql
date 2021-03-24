4c4bd544ed53b407cfd0e9f4749324955971eb40
Umar Khalilov <ERMASTER100@gmail.com>
2021-03-24 16:58:10 +0200

    Add some tasks

--- fd_lesson.sql
+++ fd_lesson.sql
@@ -42,3 +42,37 @@ brand
 FROM phones
 GROUP BY brand
 HAVING sum(quantity) > 70000;
+
+SELECT id,
+max(char_length(concat("firstName", ' ', "lastName"))) AS l
+FROM "users" 
+GROUP BY id
+ORDER BY l DESC
+LIMIT 1;
+
+SELECT
+char_length(concat("firstName", ' ', "lastName")) AS "name length",
+count(*) AS "AMOUNT"
+FROM "users"
+GROUP BY "name length"
+HAVING char_length(concat("firstName", ' ', "lastName")) < 18;
+
+SELECT char_length("email") AS "length", count(*)
+FROM "users"
+-- WHERE "email" LIKE 'm%'
+GROUP BY "length"
+-- HAVING char_length("email") >= 25
+HAVING count(*)>= 10
+ORDER BY "length";
+
+-- Извлечь все телефоны конкретного заказа
+SELECT  phones.id
+FROM  phones
+JOIN phones_to_orders ON phones_to_orders."orderId" = phones.id
+JOIN orders ON  orders.id = phones_to_orders."orderId" 
+WHERE orders.id = 2
+ORDER BY orders.id;
+
+-- Кол-во заказов каждого пользователя и его имеил
+SELECT *
+FROM users
